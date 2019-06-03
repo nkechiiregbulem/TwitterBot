194531ae1398bdf86e88bb07b269d31f0c44fdd3
NIregbulem <soccergirl0000123@gmail.com>
2019-06-03 00:37:03 -0700

    Update Post_Tweet.py

--- Post_Tweet.py
+++ Post_Tweet.py
@@ -1,3 +1,16 @@
 #Install TwitterAPI - pip install TwitterAPI
 
-from 
+from TwitterAPI import TwitterAPI
+
+access_token = "Access Token"
+access_token_secret = "Access Token Secret"
+Consumer_Key = "Consumer Key"
+Consumer_Secret = "Consumer Secret"
+
+
+api = TwitterAPI(consumer_key, consumer_secret, access_token, access_token_secret)
+
+r = api.request('statuses/update', {'status':'Insert your tweet'})
+print(r.status_code)
+
+#Status code should be returned as 200 or 403
