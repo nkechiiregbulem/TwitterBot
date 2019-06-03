49e7f3cd34211a7e024a16da0a8a57b7e7120196
NIregbulem <soccergirl0000123@gmail.com>
2019-06-03 00:14:34 -0700

    Update Retweet_Bot.py

--- Retweet_Bot.py
+++ Retweet_Bot.py
@@ -1,1 +1,30 @@
-# Quantitative Finance (Derivatives Pricing Theory) (Black-Scholes Formula)
+# First install Tweepy - pip install tweepy
+
+
+import tweepy
+
+consumer_key = "Consumer Key"
+consumer_secret = "Consumer Secret"
+access_token = "Access Token"
+access_token_secret = "Access Token Secret"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+user = api.me()
+print(user.name)
+
+def main():
+    search = ("downlighting")
+    
+    numberofTweets = 5
+    for tweet in tweepy.Cursor(api.search, search).items(numberofTweets):
+        try:
+            tweet.retweet()
+            print("Tweet Retweeted")
+        except tweepy.TweetError as e:
+            print(e.reason)
+        except StopIteration:
+            break
+main()
